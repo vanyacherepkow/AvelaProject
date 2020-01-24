74c8dda3ba3ccc523729b569fad186c4f6fbb926
vanyacherepkow <36881572+vanyacherepkow@users.noreply.github.com>
2020-01-25 01:28:06 +0300

    Initial Commit

---
+++ AvelaDemo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AvelaDemo2
+//
+//  Created by Иван on 25.01.2020.
+//  Copyright © 2020 vanyacherepkow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
